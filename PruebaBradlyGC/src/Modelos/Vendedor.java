1c8d7bb16701f40d02235ca1974c5c1609e9f1d5
BradlyGutierrez <bradly.gutierrez14520@est.uca.edu.ni>
2022-11-22 16:05:11 -0600

    Agregué el proyectoNetBeans

---
+++ PruebaBradlyGC/src/Modelos/Vendedor.java
@@ -0,0 +1,33 @@
+package Modelos;
+
+
+/**
+ * @author bradl
+ * @version 1.0
+ * @created 22-Nov-2022 3:48:48 PM
+ */
+public class Vendedor {
+
+	private int codigo;
+	private String nombres;
+	private String apellidos;
+
+	public Vendedor(){
+
+	}
+	public void finalize(){
+
+	}
+
+	public void vendedor(){
+
+	}
+
+    public Vendedor(int codigo, String nombres, String apellidos) {
+        this.codigo = codigo;
+        this.nombres = nombres;
+        this.apellidos = apellidos;
+    }
+        
+        
+}//end Vendedor
